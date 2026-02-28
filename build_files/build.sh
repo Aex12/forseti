0fac6339e6906529cc9d229c1bea60cd864f4c82
Alex Caston (Aex12) <alexcastonmolero@gmail.com>
2026-02-28 03:43:23 +0100

    feat: add fd-find package

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 dnf5 install -y mpv qbittorrent
 
 # command line tools (avoid installing weak deps such as nodejs)
-dnf5 install -y neovim ripgrep inotify-tools yq --setopt=install_weak_deps=False
+dnf5 install -y neovim ripgrep inotify-tools yq fd-find --setopt=install_weak_deps=False
 
 /ctx/utils/github-release.sh "$(cat /ctx/github-releases/helium.yaml)"
 # /ctx/utils/github-release.sh "$(cat /ctx/github-releases/sops.yaml)"
